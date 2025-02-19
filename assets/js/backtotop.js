fedcf80f33c7443287f500a241815b6ebde85a64
Romilio Solis <masterenherramientasyservicios@gmail.com>
2025-02-19 12:55:25 -0500

    Agregado botones de redes sociales

--- assets/js/backtotop.js
+++ assets/js/backtotop.js
@@ -1,16 +1,18 @@
-window.onscroll = function() {
-    const backToTop = document.getElementById('backToTop');
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      backToTop.style.display = 'flex';
-    } else {
-      backToTop.style.display = 'none';
-    }
-  };
+document.addEventListener('DOMContentLoaded', function() {
+  const backToTop = document.getElementById('backToTop');
   
-  document.getElementById('backToTop').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+  window.addEventListener('scroll', function() {
+      if (window.pageYOffset > 100) {
+          backToTop.style.display = 'flex';
+      } else {
+          backToTop.style.display = 'none';
+      }
   });
+
+  backToTop.addEventListener('click', function() {
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      });
+  });
+});
